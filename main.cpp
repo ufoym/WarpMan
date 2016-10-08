7cc91cb7fc7420a7c3d4bdee28a36825179cdd59
Ming <a@ufoym.com>
2016-10-09 00:28:56 +0800

    Add workable warpper

--- main.cpp
+++ main.cpp
@@ -3,18 +3,37 @@
 std::vector<cv::Point2f> init_bezigon() {
 	std::vector<cv::Point2f> bezigon;
 	bezigon.push_back(cv::Point2f(100, 100));
-	bezigon.push_back(cv::Point2f(200, 200));
-	bezigon.push_back(cv::Point2f(200, 200));
+	bezigon.push_back(cv::Point2f(200, 100));
+	bezigon.push_back(cv::Point2f(200, 100));
 	bezigon.push_back(cv::Point2f(300, 100));
-	bezigon.push_back(cv::Point2f(200, 200));
-	bezigon.push_back(cv::Point2f(200, 200));
+	bezigon.push_back(cv::Point2f(300, 200));
+	bezigon.push_back(cv::Point2f(300, 200));
 	bezigon.push_back(cv::Point2f(300, 300));
-	bezigon.push_back(cv::Point2f(200, 200));
-	bezigon.push_back(cv::Point2f(200, 200));
+	bezigon.push_back(cv::Point2f(200, 300));
+	bezigon.push_back(cv::Point2f(200, 300));
 	bezigon.push_back(cv::Point2f(100, 300));
-	bezigon.push_back(cv::Point2f(200, 200));
-	bezigon.push_back(cv::Point2f(200, 200));
+	bezigon.push_back(cv::Point2f(100, 200));
+	bezigon.push_back(cv::Point2f(100, 200));
 	return bezigon;
+}
+
+cv::Point2f bezier(const std::vector<cv::Point2f> &bezigon, int i, float t)
+{
+	const int n_pts = bezigon.size();
+	float t2 = t * t;
+	float t3 = t2 * t;
+	float nt = 1 - t;
+	float nt2 = nt * nt;
+	float nt3 = nt2 * nt;
+	float x = bezigon[i + 0].x * nt3
+		+ bezigon[i + 1].x * 3 * nt2 * t
+		+ bezigon[i + 2].x * 3 * nt * t2
+		+ bezigon[(i + 3) % n_pts].x * t3;
+	float y = bezigon[i + 0].y * nt3
+		+ bezigon[i + 1].y * 3 * nt2 * t
+		+ bezigon[i + 2].y * 3 * nt * t2
+		+ bezigon[(i + 3) % n_pts].y * t3;
+	return cv::Point2f(x, y);
 }
 
 void draw_bezigon(
@@ -27,28 +46,13 @@ void draw_bezigon(
 	const int n_samples = 100)
 {
 	const int n_pts = bezigon.size();
-	float prev_x = 0, prev_y = 0;
+	cv::Point2f prev_pt;
 	for (int i = 0; i < n_pts; i += 3) {
 		for (float t = 0; t < 1; t += 1.0f / n_samples) {
-			float t2 = t * t;
-			float t3 = t2 * t;
-			float nt = 1 - t;
-			float nt2 = nt * nt;
-			float nt3 = nt2 * nt;
-			float x = bezigon[i + 0].x * nt3
-				+ bezigon[i + 1].x * 3 * nt2 * t
-				+ bezigon[i + 2].x * 3 * nt * t2
-				+ bezigon[(i + 3) % n_pts].x * t3;
-			float y = bezigon[i + 0].y * nt3
-				+ bezigon[i + 1].y * 3 * nt2 * t
-				+ bezigon[i + 2].y * 3 * nt * t2
-				+ bezigon[(i + 3) % n_pts].y * t3;
-			if (t > 0) {
-				cv::line(img, cv::Point(x, y), cv::Point(prev_x, prev_y),
-					curve_color, thickness, cv::LINE_AA);
-			}
-			prev_x = x;
-			prev_y = y;
+			cv::Point2f pt = bezier(bezigon, i, t);
+			if (t > 0)
+				cv::line(img, pt, prev_pt, curve_color, thickness, cv::LINE_AA);
+			prev_pt = pt;
 		}
 	}
 	if (ctrl_alpha > 0) {
@@ -86,9 +90,42 @@ void redraw(const cv::Mat & src_img, const std::vector<cv::Point2f> & bezigon) {
 }
 
 
+cv::Mat warp(
+	const cv::Mat & src_img,
+	const std::vector<cv::Point2f> & bezigon)
+{
+	std::vector<cv::Point2f> src_pts, dst_pts, rect_pts;
+	rect_pts.push_back(cv::Point2f(0, 0));
+	rect_pts.push_back(cv::Point2f(src_img.cols, 0));
+	rect_pts.push_back(cv::Point2f(src_img.cols, src_img.rows));
+	rect_pts.push_back(cv::Point2f(0, src_img.rows));
+
+	const int n_samples = 10;
+	std::vector<cv::DMatch> matches;
+	for (int i = 0; i < bezigon.size(); i += 3) {
+		for (float t = 0; t < 1; t += 1.0f / n_samples) {
+			cv::Point2f bezigon_pt = bezier(bezigon, i, t);
+			cv::Point2f rect_pt = rect_pts[i / 3] * (1 - t) + rect_pts[(i / 3 + 1) % 4] * t;
+			matches.push_back(cv::DMatch(src_pts.size(), dst_pts.size(), 0));
+			src_pts.push_back(bezigon_pt);
+			dst_pts.push_back(rect_pt);
+		}
+	}
+
+	cv::Ptr<cv::ThinPlateSplineShapeTransformer> tps
+		= cv::createThinPlateSplineShapeTransformer(0);
+	tps->estimateTransformation(dst_pts, src_pts, matches);
+
+	cv::Mat dst_img;
+	tps->warpImage(src_img, dst_img);
+	return dst_img;
+}
+
+
 cv::Mat src_img = cv::imread("test.jpg");
 std::vector<cv::Point2f> bezigon = init_bezigon();
 int current_idx = -1;
+
 
 void mouse_handler(int event, int x, int y, int flags, void* param)
 {
@@ -106,6 +143,9 @@ void mouse_handler(int event, int x, int y, int flags, void* param)
 		break;
 	case cv::EVENT_LBUTTONUP:
 		current_idx = -1;
+		cv::Mat dst_img = warp(src_img, bezigon);
+		cv::pyrDown(dst_img, dst_img);
+		cv::imshow("result", dst_img);
 		break;
 	}
 	redraw(src_img, bezigon);
@@ -117,32 +157,6 @@ int main()
 	cv::namedWindow("source");
 	cv::setMouseCallback("source", mouse_handler, NULL);
 	redraw(src_img, bezigon);
-
-	std::vector<cv::Point2f> src_pts;
-	src_pts.push_back(cv::Point2f(0, 0));
-	src_pts.push_back(cv::Point2f(1, 0));
-	src_pts.push_back(cv::Point2f(1, 1));
-	src_pts.push_back(cv::Point2f(0, 1));
-
-	std::vector<cv::Point2f> dst_pts;
-	dst_pts.push_back(cv::Point2f(0, 0));
-	dst_pts.push_back(cv::Point2f(1, 0));
-	dst_pts.push_back(cv::Point2f(2, 1));
-	dst_pts.push_back(cv::Point2f(0, 1));
-
-	std::vector<cv::DMatch> matches;
-	for (int i = 0; i < src_pts.size(); ++i)
-		matches.push_back(cv::DMatch(i, i, 0));
-
-	cv::Ptr<cv::ThinPlateSplineShapeTransformer> tps 
-		= cv::createThinPlateSplineShapeTransformer(0);
-	tps->estimateTransformation(src_pts, dst_pts, matches);
-
-	cv::Mat dst_img;
-	tps->warpImage(src_img, dst_img);
-
-	cv::pyrDown(dst_img, dst_img);
-	cv::imshow("result", dst_img);
 	cv::waitKey(0);
 	return 0;
 }
